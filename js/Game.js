7ad28818329bf408ccebe5e997981fa095d53014
ifaxec <xecfahim.ahmed@gmail.com>
2022-12-23 21:58:53 +0000

    updated game.js

--- js/Game.js
+++ js/Game.js
@@ -6,11 +6,11 @@ class Game {
     constructor(){
         this.missed = 0;
         this.phrases = [
-            {"That which does not kill us makes us stronger"},
-            {"The journey of a thousand miles begins with one step"},
-            {"Fortune favors the bold"},
-            {"Time is money"},
-            {"Practice makes perfect"}
+            new Phrase ("That which does not kill us makes us stronger"),
+            new Phrase ("The journey of a thousand miles begins with one step"),
+            new Phrase ("Fortune favors the bold"),
+            new Phrase ("Time is money"),
+            new Phrase ("Practice makes perfect")
         ];
         this.activePhrase = "null";
     }
--- js/app.js
+++ js/app.js
@@ -2,13 +2,15 @@
  * Project 4 - OOP Game App
  * app.js */
 
-const phrase = new Phrase();
+//const phrase = new Phrase();
 const game = new Game();
 
 //Testing
 //const phrase = new Phrase("Life is like a box of chocolate");
 //console.log(`Phrase - phrase: ${phrase.phrase}`)
 
-game.phrases.forEach((phrase, index) => {
+/**
+ * game.phrases.forEach((phrase, index) => {
         console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
         });
+*/
